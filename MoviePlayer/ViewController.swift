8a6e8930bb3d7a36ba1511fe1e7f3df60340e009
choccho <shocho0101@hotmail.co.jp>
2020-02-06 19:54:02 +0900

    Initial Commit

---
+++ MoviePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by 張翔 on 2020/02/06.
+//  Copyright © 2020 張翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
